0673286a24edfa74c9a6f7f0c029790703a1e906
hualuomoli <liubaoquan0831@163.com>
2016-06-16 10:06:24 +0800

    update

--- creator/start.sh
+++ creator/start.sh
@@ -4,4 +4,4 @@ cur_path=$(cd "$(dirname "$0")"; pwd)
 echo $cur_path
 
 
-node $cur_path/app.js >> $cur_path/log.log 2>&1 &
+node $cur_path/app.js --port 3000 --scriptSuffix sh >> $cur_path/log.log 2>&1 &
